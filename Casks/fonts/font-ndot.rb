f8d9a349a81a9587f97142a7e875c9225d895691
Souma <101255979+5ouma@users.noreply.github.com>
2025-06-21 12:39:57 +0900

    fix(fonts): Update the Ndot font URL
    Nothing has changed its website structure.

--- Casks/fonts/font-ndot.rb
+++ Casks/fonts/font-ndot.rb
@@ -2,7 +2,7 @@ cask "font-ndot" do
   version :latest
   sha256 :no_check
 
-  url "https://nothing.tech/cdn/shop/t/33/assets/Ndot-55.otf"
+  url "https://cdn.shopify.com/oxygen-v2/43495/38485/80939/1952643/fonts/Ndot-55.otf"
   name "Ndot"
   desc "Nothing original pixel font in Nothing OS"
   homepage "https://nothing.tech/"
