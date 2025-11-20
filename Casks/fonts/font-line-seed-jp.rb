21a4f3aa9a91a5576212c47867888913aa3aed5b
Souma <101255979+5ouma@users.noreply.github.com>
2025-11-20 10:42:53 +0900

    fix(fonts): Update the LINE Seed font path
    Upstream has changed its directory strucutre.

--- Casks/fonts/font-line-seed-jp.rb
+++ Casks/fonts/font-line-seed-jp.rb
@@ -1,15 +1,15 @@
 cask "font-line-seed-jp" do
-  version "20241007"
-  sha256 "1e885e9bff2b68647576ff50cdd7f0d69f3776ba3af0f10052dd122aca57e5ff"
+  version "20251119"
+  sha256 "97019208d3b6886d5fc584bf5285e09332271247786dd9f9b8d96964812a6290"
 
-  url "https://github.com/line/seed/releases/download/v#{version}/LINESeed-fonts.zip",
+  url "https://github.com/line/seed/releases/download/v#{version}/seed-v#{version}.zip",
       verified: "github.com/"
   name "LINE Seed JP"
   desc "Seed, a word containing the meaning of growth, is LINE's first custom typeface"
   homepage "https://seed.line.me/"
 
-  font "LINESeed-fonts/LINESeedJP/fonts/otf/LINESeedJP-Bold.otf"
-  font "LINESeed-fonts/LINESeedJP/fonts/otf/LINESeedJP-ExtraBold.otf"
-  font "LINESeed-fonts/LINESeedJP/fonts/otf/LINESeedJP-Regular.otf"
-  font "LINESeed-fonts/LINESeedJP/fonts/otf/LINESeedJP-Thin.otf"
+  font "seed-v#{version}/LINESeedJP/fonts/otf/LINESeedJP-Bold.otf"
+  font "seed-v#{version}/LINESeedJP/fonts/otf/LINESeedJP-ExtraBold.otf"
+  font "seed-v#{version}/LINESeedJP/fonts/otf/LINESeedJP-Regular.otf"
+  font "seed-v#{version}/LINESeedJP/fonts/otf/LINESeedJP-Thin.otf"
 end
