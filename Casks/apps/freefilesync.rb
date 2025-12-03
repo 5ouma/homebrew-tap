c61c5b899353ca2736df4cb8736a35c812af2fc1
Souma <101255979+5ouma@users.noreply.github.com>
2025-12-03 01:04:19 +0000

    freefilesync 14.6

--- Casks/apps/freefilesync.rb
+++ Casks/apps/freefilesync.rb
@@ -1,6 +1,6 @@
 cask "freefilesync" do
-  version "14.5"
-  sha256 "99b531404a083b25bf323750c5e376fca9aae7af491a2872e3b11d3789f5b273"
+  version "14.6"
+  sha256 "b0a28dfbefc08877a688eb83d05b956f93d973a6501fb5c24b9c9ebd3a6fe933"
 
   url "https://freefilesync.org/download/FreeFileSync_#{version}_macOS.zip"
   name "FreeFileSync"
