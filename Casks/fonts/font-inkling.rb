923d57df65c09dbccc57b564d1fcd48463c52f7d
Souma <git@5ouma.me>
2026-01-29 10:39:10 +0900

    chore(fonts): Implement Inkling font livecheck
    Check the latest version to keep up-to-date.

--- Casks/fonts/font-inkling.rb
+++ Casks/fonts/font-inkling.rb
@@ -1,11 +1,16 @@
 cask "font-inkling" do
-  version "2.000"
-  sha256 :no_check
+  version "2.0"
+  sha256 "d41c95f5cfd6e59af1414fa1ecb0b0a3f986bf135376e4dfff1f3495a818a843"
 
-  url "http://frozenpandaman.github.io/Inkling_v2.otf"
+  url "http://frozenpandaman.github.io/Inkling_v#{version.major}.otf"
   name "Inkling"
   desc "Font created from characters that appear in the game Splatoon"
   homepage "https://frozenpandaman.github.io/inkling.html"
+
+  livecheck do
+    url :homepage
+    regex(/VERSION (\d+\.\d+)/)
+  end
 
   font "Inkling_v2.otf"
 end
