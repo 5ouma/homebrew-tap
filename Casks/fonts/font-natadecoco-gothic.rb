8349414c408544dd6b006f0e82fda716774fd5ec
Souma <git@5ouma.me>
2026-01-31 21:47:18 +0900

    fix(fonts): Update the upstream URL change
    The domain has been changed.

--- Casks/fonts/font-natadecoco-gothic.rb
+++ Casks/fonts/font-natadecoco-gothic.rb
@@ -2,10 +2,10 @@ cask "font-natadecoco-gothic" do
   version :latest
   sha256 :no_check
 
-  url "https://kashika-labo.com/download/natadecoco-gothic-zip/?wpdmdl=1295"
+  url "https://kashika-lab.co.jp/wp-content/uploads/2025/08/Natadecoco-gothic_f.zip"
   name "Natadecoco gothic"
   desc "柔らかな幾何学図形が固まって生まれた優しい欧文フリーフォント"
-  homepage "https://kashika-labo.com/natadecoco-gothic"
+  homepage "https://kashika-lab.co.jp/natadecoco-gothic"
 
-  font "Natadecoco-gothic/Natadecoco-gothic.otf"
+  font "Natadecoco-gothic_f/Natadecoco-gothic.otf"
 end
